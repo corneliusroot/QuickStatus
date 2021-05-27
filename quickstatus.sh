b5ffa7aa544d8e99b242e266f70dab31ee3733f1
corneliusroot <geocrasher@gmail.com>
2021-05-26 22:51:33 -0700

    Update quickstatus.sh
    Added dovecot on port 110 and Postfix on 25 and several incremental changes made outside of github.

--- quickstatus.sh
+++ quickstatus.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-############################################
-# Quick Status v.1
-# Created by Ryan Flowers
-# https://github.com/corneliusroot
-# Licensed under the MIT License
+#-------------------------------------------------------
+#Quick Diag v.2
+#Created by Ryan Flowers for ASO 9/30/2014
+#Please report all bugs to ryan.flowers@asmallorange.com
+#Please include server details and account details so
+#that I can reproduce the problem.
 #
-# Usage: sh quickinfo.sh
 #
-############################################
+#Usage: sh quickdiag.sh
+# 05/26/21 Added dovecot on port 110 and Postfix on 25
+#-------------------------------------------------------
 
 
-# Define colors for use in script
+
+# Getting all colorful for this one
 red='\e[0;31m'
 grn='\e[0;32m'
 yel='\e[0;93m'
 NC='\e[0m' # No Color
 
 #Get uptime and reformat it to something easy to read
-uptime=$(uptime	-p | sed 's/^up	//')
+uptime=$(uptime -p | sed 's/^up //')
 
 ###### File System Check function
 checkfs (){
@@ -93,16 +96,20 @@ printf "%-15s ${capco}%3s%%${NC} %-18s [${fsw}%-10s${NC}] [${capco}%10s${NC}] [$
 
 done
 }
-# END File system checking
+########### END File system checking
 
 
 
 
-# BEGIN Web Services checking
+########## BEGIN Web Services checking
 checkweb (){
 # Find out what web server is in use
+#port80=`lsof -P -i:80 | awk '{ print $1 }' | head -1`
+#port8080=`lsof -P -i:8080 | awk '{ print $1 }' | head -1`
 port80=`lsof -P -i:80 | awk '{ print $1 }' | sort | uniq -c | sort -n | tail -1 | awk '{ print $2 }'`
 port8080=`lsof -P -i:8080 | awk '{ print $1 }' | sort | uniq -c | sort -n | tail -1 | awk '{ print $2 }'`
+#echo $port80
+#echo $port8080
 
 #if 80 is dead, raise a red flag and stop checking
 if [ -z ${port80} ]
@@ -137,24 +144,39 @@ fi
 ##### Function: Postfix queue check
 ebpc=$(mailq | tail -1 | awk '{ print $5 }')
 if [ -z $ebpc ]
-	then
-	cebpc="${grn}Empty${NC}"
-	else
-		if test ${ebpc} -le 1999
-		        then
-        		        cebpc="${grn}${ebpc}${NC}"
-	        	else
-                	cebpc="${red}${ebpc}${NC}"
-		fi
+        then
+        cebpc="${grn}Empty${NC}"
+        else
+                if test ${ebpc} -le 1999
+                        then
+                                cebpc="${grn}${ebpc}${NC}"
+                        else
+                        cebpc="${red}${ebpc}${NC}"
+                fi
 fi
 ##### End Function: Postfix queue check
 
-# MySQL check
 if [ "$(lsof -i:3306 | awk '{ print $1}' | tail -1)" == "mysqld" ]
         then
                 cmysql="${grn}MySQL OK${NC}"
         else
                 cmysql="${red}MySQL DOWN${NC}"
+
+fi
+
+if [ "$(lsof -i:110 | awk '{ print $1}' | tail -1)" == "dovecot" ]
+        then
+                cdovecot="${grn}Dovecot OK${NC}"
+        else
+                cdovecot="${red}Dovecot DOWN${NC}"
+
+fi
+
+if [ "$(lsof -i:25 | awk '{ print $1}' | tail -1)" == "smtpd" ]
+        then
+                cpostfix="${grn}Postfix OK${NC}"
+        else
+                cpostfix="${red}Postfix DOWN${NC}"
 
 fi
 
@@ -164,6 +186,12 @@ echo "Hostname      "`hostname`
 echo "Uptime:       "${uptime}
 checkweb
 echo -e "MySQL:        "${cmysql}
+echo -e "Postfix:      "${cpostfix}
+echo -e "Dovecot:      "${cdovecot}
+
 echo -e "Mail Queue:   "${cebpc}
+
+#echo "Disk Status:  "${}
 echo "--------------------------------"
+#checkfs |sed -e 's#/dev/mapper/#/#' -e 's/VolGroup/VG/' -e 's/LogVol/LV/'
 checkfs
